dc0cd319cfa3c956c33c4611ceb446778dc1151d
PradeepAwate <pradeep.awate8@gmail.com>
2021-02-06 12:15:31 +0530

    svg added

--- src/logged_out/components/home/FeatureSection.js
+++ src/logged_out/components/home/FeatureSection.js
@@ -1,41 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Grid, Typography, isWidthUp, withWidth } from "@material-ui/core";
-import BuildIcon from "@material-ui/icons/Build";
-import ComputerIcon from "@material-ui/icons/Computer";
-import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
-import MeassageIcon from "@material-ui/icons/Message";
 import calculateSpacing from "./calculateSpacing";
 import FeatureCard from "./FeatureCard";
-
-const iconSize = 30;
+import { ReactComponent as HealthyMeal } from "../../../icons/healthy-eating.svg";
+import { ReactComponent as DeliveryMan } from "../../../icons/delivery-man.svg";
+import { ReactComponent as Nutritionist } from "../../../icons/nutritionist.svg";
+import { ReactComponent as Safe } from "../../../icons/safe.svg";
 
 const features = [
   {
     color: "#00C853",
-    headline: "Tasty & Healthy Meals",
-    icon: <BuildIcon style={{ fontSize: iconSize }} />,
+    headline: "Customised Meal",
+    icon: <HealthyMeal style={{ width: "100px", height: "90px" }}/>,
     mdDelay: "0",
     smDelay: "0",
   },
   {
     color: "#6200EA",
     headline: "Doorstep Delivery",
-    icon: <CalendarTodayIcon style={{ fontSize: iconSize }} />,
+    icon: <DeliveryMan style={{ width: "100px", height: "90px" }} />,
     mdDelay: "200",
     smDelay: "200",
   },
   {
     color: "#0091EA",
     headline: "Personalized Nutritionist",
-    icon: <MeassageIcon style={{ fontSize: iconSize }} />,
+    icon: <Nutritionist style={{ width: "100px", height: "90px" }}/>,
     mdDelay: "400",
     smDelay: "0",
   },
   {
     color: "#d50000",
     headline: "100% Safe & Hygenic",
-    icon: <ComputerIcon style={{ fontSize: iconSize }} />,
+    icon: <Safe style={{ width: "100px", height: "90px" }} />,
     mdDelay: "0",
     smDelay: "200",
   },
@@ -50,7 +48,10 @@ function FeatureSection(props) {
           What You Get
         </Typography>
         <div className="container">
-          <Grid container spacing={calculateSpacing(width)}>
+          <Grid container spacing={calculateSpacing(width)} 
+                style={{
+                  textAlign: "center"
+                }}>
             {features.map((element) => (
               <Grid
                 item
