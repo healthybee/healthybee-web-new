0cd47acf589305cbd9a3efcc2248ded87838b7a2
PradeepAwate <pradeep.awate8@gmail.com>
2021-03-27 20:11:26 +0530

    subscription-form

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,3 +3,41 @@ export const API_URL = {
   USERS: "/users",
   FEEDBACK: "/feedbacks",
 };
+
+export const mealpref = [
+  { value: "veg", label: "Veg" },
+  { value: "nonveg", label: "Non-Veg" },
+  { value: "vegan", label: "Vegan" },
+  { value: "eggeterian", label: "Eggeterian" }
+];
+
+export const fitnessoptions = [
+  { value: "weightloss", label: "Weight Loss" },
+  { value: "weightgain", label: "Weight Gain" }
+];
+
+export const dietpref = [
+  { value: "keto", label: "Keto" },
+  { value: "lowcarb", label: "Low Carb" },
+  { value: "highprotein", label: "High Protein" },
+  { value: "gmdiet", label: "GM diet" }
+];
+
+export const meals = [
+  { value: "breakfast", label: "Breakfast" },
+  { value: "lunch", label: "Lunch" },
+  { value: "dinner", label: "Dinner" }
+];
+
+export const mealduration = [
+  { value: "15", label: "15" },
+  { value: "30", label: "30" },
+  { value: "90", label: "90" }
+];
+
+export const physicalactivity = [
+  { value: "cardio", label: "Cardiovascular" },
+  { value: "strength", label: "Strength" },
+  { value: "flexi", label: "Flexibility" }
+];
+
--- src/logged_out/components/subscription_plans/SubscriptionPlans.js
+++ src/logged_out/components/subscription_plans/SubscriptionPlans.js
@@ -1,199 +1,244 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import SettingsIcon from '@material-ui/icons/Settings';
-import GroupAddIcon from '@material-ui/icons/GroupAdd';
-import VideoLabelIcon from '@material-ui/icons/VideoLabel';
-import StepConnector from '@material-ui/core/StepConnector';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import Header from "./subscription_forms/Header";
+import ReactDatePicker from "react-datepicker";
+import ReactSelect from "react-select";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  mealpref,
+  fitnessoptions,
+  dietpref,
+  meals,
+  mealduration,
+  physicalactivity
+} from "../../../constants/constants";
+import {
+  RadioGroup,
+  FormControlLabel,
+  ThemeProvider,
+  Radio,
+  createMuiTheme,
+  Grid
+} from "@material-ui/core";
+import "react-datepicker/dist/react-datepicker.css";
+import ButtonsResult from "./subscription_forms/ButtonResult";
+import "./subscription.css";
 
-const ColorlibConnector = withStyles({
-  alternativeLabel: {
-    top: 22,
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
   },
-  active: {
-    '& $line': {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+  overrides: {
+    MuiRadio: {
+      root: {
+        color: 'white',
+      },
+      colorSecondary: {
+        '&$checked': {
+          color: 'white',
+        },
+      },
     },
-  },
-  completed: {
-    '& $line': {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-    },
-  },
-  line: {
-    height: 3,
-    border: 0,
-    backgroundColor: '#eaeaf0',
-    borderRadius: 1,
-  },
-})(StepConnector);
-
-const useColorlibStepIconStyles = makeStyles({
-  root: {
-    backgroundColor: '#ccc',
-    zIndex: 1,
-    color: '#fff',
-    width: 50,
-    height: 50,
-    display: 'flex',
-    borderRadius: '50%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  active: {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
-  },
-  completed: {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
   },
 });
 
-function ColorlibStepIcon(props) {
-  const classes = useColorlibStepIconStyles();
-  const { active, completed } = props;
-
-  const icons = {
-    1: <SettingsIcon />,
-    2: <GroupAddIcon />,
-    3: <VideoLabelIcon />,
-  };
-
-  return (
-    <div
-      className={clsx(classes.root, {
-        [classes.active]: active,
-        [classes.completed]: completed,
-      })}
-    >
-      {icons[String(props.icon)]}
-    </div>
-  );
-}
-
-ColorlibStepIcon.propTypes = {
-  /**
-   * Whether this step is active.
-   */
-  active: PropTypes.bool,
-  /**
-   * Mark the step as completed. Is passed to child components.
-   */
-  completed: PropTypes.bool,
-  /**
-   * The label displayed in the step icon.
-   */
-  icon: PropTypes.node,
-};
-
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    marginTop: '100px',
-    marginBottom: '50px',
-    marginLeft: '50px'
-  },
-  button: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
+  container: {
+    margin: 0
+  }
 }));
 
-function getSteps() {
-  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
-}
-
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return 'Select campaign settings...';
-    case 1:
-      return 'What is an ad group anyways?';
-    case 2:
-      return 'This is the bit I really care about!';
-    default:
-      return 'Unknown step';
-  }
-}
+const defaultValues = {
+  Name: "",
+  Phoneno: "",
+  Age: "",
+  Weight: "",
+  PhysicalActivity: { value: "", label: "" },
+  MealPref: { value: "veg", label: "Veg" },
+  FitnessOption: { value: "weightloss", label: "Weight Loss" },
+  DietPref: { value: "keto", label: "Keto" },
+  Meals: { value: "breakfast", label: "Breakfast" },
+  MealDuration: { value: "15", label: "15" },
+  RadioGroup: ""
+};
 
 function Subscription(props) {
+  const { handleSubmit, register, reset, control } = useForm({ defaultValues });
+  const [data, setData] = useState(null);
   const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const steps = getSteps();
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
   return (
-    <div className={classes.root}>
-      <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <div>
-        {activeStep === steps.length ? (
-          <div>
-            <Typography className={classes.instructions}>
-              All steps completed - you&apos;re finished
-            </Typography>
-            <Button onClick={handleReset} className={classes.button}>
-              Reset
-            </Button>
-          </div>
-        ) : (
-          <div>
-            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-            <div>
-              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
-                Back
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleNext}
-                className={classes.button}
-              >
-                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-              </Button>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <form onSubmit={handleSubmit((data) => setData(data))} className="form">
+        <Header/>
+        <Grid container xs={12} spacing={3} className={classes.container}>
+          <Grid item xs={12} className="formsectionheading"><section><label>Fitness Goal:</label></section></Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Fitness Goal</label>
+              <Controller
+                as={ReactSelect}
+                options={fitnessoptions}
+                name="FitnessOption"
+                isClearable
+                control={control}
+              />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Diet Preference</label>
+              <Controller
+                as={ReactSelect}
+                options={dietpref}
+                name="DietPref"
+                isClearable
+                control={control}
+              />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Meal Preference</label>
+              <Controller
+                as={ReactSelect}
+                options={mealpref}
+                name="MealPref"
+                isClearable
+                control={control}
+              />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Meals</label>
+              <Controller
+                as={ReactSelect}
+                options={meals}
+                name="Meals"
+                isClearable
+                isMulti
+                control={control}
+              />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Meal Duration</label>
+              <Controller
+                as={ReactSelect}
+                options={mealduration}
+                name="MealDuration"
+                isClearable
+                control={control}
+              />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Start Date</label>
+              <Controller
+                as={ReactDatePicker}
+                control={control}
+                valueName="selected" // DateSelect value's name is selected
+                onChange={([selected]) => selected}
+                name="ReactDatepicker"
+                className="input"
+                placeholderText="Select date"
+              />
+            </section>
+            </Grid>
+
+          <Grid item xs={12} className="formsectionheading"><section><label>Customer Details:</label></section></Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Name:</label>
+              <input name="Name" className="input" ref={register} />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Phone No.:</label>
+              <input name="Phoneno" className="input" ref={register} />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Age:</label>
+              <input name="Age" className="input" ref={register} />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Gender: </label>
+              <Controller
+                as={
+                  <RadioGroup aria-label="gender">
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio />}
+                      label="Female"
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio />}
+                      label="Male"
+                    />
+                  </RadioGroup>
+                }
+                name="RadioGroup"
+                control={control}
+              />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+          <section>
+            <label>Weight:</label>
+            <input name="Weight" className="input" ref={register} />
+          </section>
+          </Grid>
+            <Grid item xs={6}>
+          <section>
+            <label>Physical Activity</label>
+            <Controller
+              as={ReactSelect}
+              options={physicalactivity}
+              name="PhysicalActivity"
+              isClearable
+              control={control}
+            />
+          </section>
+          </Grid>
+          
+          <Grid item xs={12} className="formsectionheading"> <section><label>Address:</label></section> </Grid>
+            <Grid item xs={6}>
+          <section>
+            <label>Building & Flat No.:</label>
+            <input name="line0" className="input" ref={register} />
+          </section>
+          </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Line1:</label>
+              <input name="line1" className="input" ref={register} />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Line2:</label>
+              <input name="line2" className="input" ref={register} />
+            </section>
+            </Grid>
+            <Grid item xs={6}>
+            <section>
+              <label>Pin Code:</label>
+              <input name="pincode" className="input" ref={register} />
+            </section>
+            </Grid>
+        </Grid>
+        <ButtonsResult {...{ data, reset, defaultValues }} />
+      </form>
+    </ThemeProvider>
   );
 }
 
-Subscription.propTypes = {
-  classes: PropTypes.object.isRequired,
-  Icon: PropTypes.element.isRequired,
-  color: PropTypes.string.isRequired,
-  headline: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-};
-
-export default withStyles({ withTheme: true })(Subscription);
+export default (Subscription);
